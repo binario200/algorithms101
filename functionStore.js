f2c8dc2fcaef775121d3abe276d40af938012086
loko <binario200@gmail.com>
2017-02-01 23:57:09 -0600

    Update functionStore.js

--- functionStore.js
+++ functionStore.js
@@ -12,3 +12,21 @@ var store = {
     }
   }
 }
+
+/*
+
+function ninja() {}
+
+console.log("Empty store", store);
+
+store.add(ninja);
+
+console.log("storing a function", store);
+
+
+store.add(ninja);
+
+console.log("trying to store the same function", store);
+
+
+*/
